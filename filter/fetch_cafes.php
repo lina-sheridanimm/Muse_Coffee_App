da1ce923d1cb51fd3e93e46be167d0153ee3a72a
lina-sheridanimm <leedoy@sheridancollege.ca>
2024-12-06 10:30:50 -0500

    main page layout -> working on connecting filters

--- filter/fetch_cafes.php
+++ filter/fetch_cafes.php
@@ -4,23 +4,42 @@ $username = "root";
 $password = "root"; 
 $dbname = "brewmatch"; 
 
+header('Content-Type: application/json; charset=utf-8');
+
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die('connection failed'. $conn->connect_error);
 }
 
-$sql = "SELECT name FROM brewmatch_cafes";
+$sql = "SELECT DISTINCT bc.* 
+        FROM brewmatch_cafes bc
+        LEFT JOIN cafe_meals cm ON bc.cafe_id = cm.cafe_id
+        LEFT JOIN cafe_drinks cd ON bc.cafe_id = cd.cafe_id
+        LEFT JOIN cafe_good_for cg ON bc.cafe_id = cg.cafe_id
+        LEFT JOIN cafe_amenities ca ON bc.cafe_id = ca.cafe_id
+        WHERE 1";
+
+// need -> name, rating, city, distance -> cant do open/close cuz not all cafes have the hours data :'(
+
 $result = $conn->query($sql);
 
 $cafes = [];
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
-        $cafes[] = $row['name']; 
+        $cafes[] = [
+            'id' => $row['cafe_id'],
+            'name' => $row['name'],
+            'location' => [
+                'city' => "Oakville",
+                'distance' => $row['distance_category']
+            ],
+            'rating' => $row['rating']
+        ];
     }
 }
 
-echo json_encode($cafes);
+echo json_encode($cafes, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
 
 $conn->close();
 ?>
